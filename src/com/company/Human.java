37245fc94dd75db29c37ec142af968af01412233
ARTrex007 <arturmarksz7@gmail.com>
2021-06-13 02:39:07 +0200

    Zadanie 5

---
+++ src/com/company/Human.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Human
+{
+    public String imie, nazwisko, zawod, animal_pet;
+    public int wiek;
+    public double masa;
+    public static int counter=0;
+
+    public Human(String imie, String nazwisko, String zawod, int wiek, double masa, String animal_pet) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.zawod = zawod;
+        this.wiek = wiek;
+        this.masa = masa;
+        this.animal_pet = animal_pet;
+        System.out.println("Utworzono obiekt HUMAN numer: " + counter);
+        counter++;
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Animal animal1 = new Animal("Stefan", "Pies", true, 3);
+        Animal animal2 = new Animal("Ryszard", "Kot", false, 2);
+        Animal animal3 = new Animal("Daisy", "Koń", false, 10);
+
+        animal2.Take_for_a_walk();
+        animal2.Take_for_a_walk();
+        animal2.Take_for_a_walk();
+        animal2.Take_for_a_walk();
+        animal2.Take_for_a_walk();
+        animal2.Feed();
+        animal1.Feed();
+        animal3.Take_for_a_walk();
+    }
+}
