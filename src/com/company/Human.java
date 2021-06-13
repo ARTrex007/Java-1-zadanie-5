469b9dba5e47d39a3948437e50b9769a38741e17
ARTrex007 <arturmarksz7@gmail.com>
2021-06-14 00:48:34 +0200

    obiekt jako argument

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -2,18 +2,19 @@ package com.company;
 
 public class Human
 {
-    public String imie, nazwisko, zawod, animal_pet;
+    public String imie, nazwisko, zawod;
     public int wiek;
     public double masa;
     public static int counter=0;
+    public Animal animal_pet;
 
-    public Human(String imie, String nazwisko, String zawod, int wiek, double masa, String animal_pet) {
+    public Human(String imie, String nazwisko, String zawod, int wiek, double masa, Animal animal) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.zawod = zawod;
         this.wiek = wiek;
         this.masa = masa;
-        this.animal_pet = animal_pet;
+        this.animal_pet = animal;
         System.out.println("Utworzono obiekt HUMAN numer: " + counter);
         counter++;
     }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,6 +11,9 @@ public class Main
         Animal animal1 = new Animal("Stefan", "Pies", true, 3);
         Animal animal2 = new Animal("Ryszard", "Kot", false, 2);
         Animal animal3 = new Animal("Daisy", "Koń", false, 10);
+        Human human1 = new Human("janusz", "Kowalski", "Mechanik", 54, 74, animal1);
+
+        System.out.println(human1.animal_pet.imie);
 
         animal2.Take_for_a_walk();
         animal2.Take_for_a_walk();
